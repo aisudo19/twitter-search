21e2170362355fe8216d471c89c0baa68e3fa01b
ai.sudo <ai.sudo@kentokento.com>
2022-08-17 14:10:42 +0900

    指定した日付より古いツイートが出ないようバグ修正

--- search/scraping.py
+++ search/scraping.py
@@ -7,13 +7,13 @@ HEADER      = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) G
 
 #グーグル検索から検索結果のリストを返す関数
 def search_twitter(words, since_date, until_date, search_limit):
-    print("since: " + since_date + " until: " + until_date)
+    # print("since: " + since_date + " until: " + until_date)
     if(search_limit == ""):
         search_limit = 10
     result_raw = []
     tweets_list = []
-    query = " since:" + since_date + " until:" + until_date + " lang:ja -filter:links -filter:replies"
-    print(query)
+    query = " since:" + since_date + "_00:00:00_JST until:" + until_date + "_23:59:59_JST lang:ja -filter:links -filter:replies"
+    # print(query)
     try:
         result_raw = enumerate(sntwitter.TwitterSearchScraper(words + query).get_items())
     except Exception as e:
@@ -22,13 +22,13 @@ def search_twitter(words, since_date, until_date, search_limit):
         for i, tweet in result_raw:
             if len(tweets_list)>int(search_limit)-1:
                 break
-            if len(tweets_list)>5000:
-                break
             
             tweetUrl = "https://twitter.com/" + tweet.user.username + "/status/" + str(tweet.id)
             date = tweet.date.strftime('%Y/%m/%d %H:%M:%S')
             tweet_date = tweet.date.strftime('%Y%m%d')
-            
+            print("tweet_date: " + tweet_date + " since_date:" + since_date.replace("-", ""))
+            if(tweet_date < since_date.replace("-", "")):
+                break
             deleteEolContent = tweet.content.replace("\r\n", "")
             deleteEolContent = tweet.content.replace("\r", "")
             deleteEolContent = tweet.content.replace("\n", "")
